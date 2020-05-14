eef753b318b775d327f7ccf2a47c38c25a8b4f6f
Maja Jabłońska <mjablonska@iterato.rs>
2020-05-14 19:11:23 +0200

    Add frontend

--- frontend/sender/routes/data.js
+++ frontend/sender/routes/data.js
@@ -1,10 +1,30 @@
 var express = require('express');
+const { Kafka } = require('kafkajs');
+const kafkaConfig = require('../config/kafkaConfig.json');
 var router = express.Router();
 
-/* POST data from CSV. */
-router.post('/', function(req, res, next) {
-  console.log(req.body);
-  res.send('POST to data');
+const kafka = new Kafka({
+    clientId: kafkaConfig.clientID,
+    brokers: kafkaConfig.brokers
 });
+const producer = kafka.producer();
+
+/* POST data from CSV. */
+router.post('/', async function(req, res, next) {
+  sendMessage(JSON.stringify(req.body));
+  res.send('POST data successful');
+});
+
+async function sendMessage(message) {
+    await producer.connect();
+    await producer.send({
+    topic: 'Messages',
+    messages: [{
+            key: 'RowData',
+            value: message
+        }],
+    });
+    await producer.disconnect();
+}
 
 module.exports = router;
